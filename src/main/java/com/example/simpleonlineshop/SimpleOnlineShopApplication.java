647758e75b0e9d61e459ae8957286a5af9be3924
Tatev <tatevgyozalyan04@gmail.com>
2025-09-11 14:11:01 +0400

    1st commit to repo

---
+++ src/main/java/com/example/simpleonlineshop/SimpleOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleonlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOnlineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleOnlineShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpleonlineshop/SimpleOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleonlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOnlineShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
