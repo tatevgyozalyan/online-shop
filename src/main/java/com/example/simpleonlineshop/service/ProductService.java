923be5aa8873bb67686aa94106ef9bcd1071f942
Tatev <tatevgyozalyan04@gmail.com>
2025-09-25 19:08:33 +0400

    Custom manual pagination fixed-1

--- src/main/java/com/example/simpleonlineshop/service/ProductService.java
+++ src/main/java/com/example/simpleonlineshop/service/ProductService.java
@@ -5,6 +5,9 @@ import com.example.simpleonlineshop.entity.Product;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,39 +21,21 @@ public class ProductService {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public List<Product> getAllProducts(int page, int size) {
-        return entityManager.createQuery("SELECT p FROM Product p ORDER BY p.id", Product.class)
-                .setFirstResult(page * size)  // OFFSET
-                .setMaxResults(size)          // LIMIT
-                .getResultList();
+    public Page<Product> getAllProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 
-    public long getTotalProducts() {
-        return entityManager.createQuery("SELECT COUNT(p) FROM Product p", Long.class)
-                .getSingleResult();
-    }
-    public List<Product> getProductsByCategory(Long categoryId, int page, int size) {
-        return entityManager.createQuery(
-                        "SELECT p FROM Product p WHERE p.category.id = :param ORDER BY p.id",
-                        Product.class)
-                .setParameter("param", categoryId)
-                .setFirstResult(page * size)
-                .setMaxResults(size)
-                .getResultList();
+    public Page<Product> getProductsByCategory(Long categoryId, Pageable pageable) {
+        return productRepository.findProductByCategoryId(categoryId, pageable);
     }
 
     public Product getProductById(Long id) {
         return productRepository.findById(id).orElse(null);
     }
 
-    public List<Product> findByProductName(String name, int page, int size) {
-        return entityManager.createQuery(
-                        "SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(:param) ORDER BY p.id",
-                        Product.class)
-                .setParameter("param", "%" + name + "%")
-                .setFirstResult(page * size)
-                .setMaxResults(size)
-                .getResultList();
+    public Page<Product> findByProductName(String name, Pageable pageable) {
+        return productRepository.findByNameContainingIgnoreCase(name, pageable);
+
     }
 
     public long getTotalProductsByName(String name) {
